6af7b078ab4d45d7f3fcd9d95c0332c7a2cb27ee
SirHall <sirhallstein@gmail.com>
2018-11-21 01:38:57 +1000

    Removed pointless 'First' extension method from LinqE

--- LinqE.cs
+++ LinqE.cs
@@ -3,23 +3,20 @@ using System.Linq;
 using System;
 
 
-namespace Excessives.LinqE
-{
+namespace Excessives.LinqE {
 	/* {TODO}
      * Fix indentations
      * Comment obscure code (Fairly obvious)
      */
 
-	public static class LinqE
-	{
+	public static class LinqE {
 		#region Loops
 
 		//Foreach, no return
 		public static IEnumerable<TSource> ForEach<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Action<TSource> action
-		)
-		{
+		) {
 			for (int i = 0; i < enumerable.Count(); i++)
 				action(enumerable.ElementAt(i));
 			return enumerable;
@@ -29,8 +26,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> For<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Action<TSource, int> action
-		)
-		{
+		) {
 			for (int i = 0; i < enumerable.Count(); i++)
 				action(enumerable.ElementAt(i), i);
 			return enumerable;
@@ -40,8 +36,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> ForEachR<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Func<TSource, TSource> action
-		)
-		{
+		) {
 			for (int i = 0; i < enumerable.Count(); i++)
 				enumerable.ToArray()[i] = action(enumerable.ElementAt(i));
 			return enumerable.AsEnumerable();
@@ -51,8 +46,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> ForR<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Func<TSource, int, TSource> action
-		)
-		{
+		) {
 			for (int i = 0; i < enumerable.Count(); i++)
 				enumerable.ToArray()[i] = action(enumerable.ElementAt(i), i);
 			return enumerable.AsEnumerable();
@@ -66,8 +60,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> ForEachBack<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Action<TSource> action
-		)
-		{
+		) {
 			for (int i = enumerable.Count() - 1; i >= 0; i--)
 				action(enumerable.ElementAt(i));
 			return enumerable;
@@ -77,8 +70,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> ForBack<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Action<TSource, int> action
-		)
-		{
+		) {
 			for (int i = enumerable.Count() - 1; i >= 0; i--)
 				action(enumerable.ElementAt(i), i);
 			return enumerable;
@@ -88,8 +80,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> ForEachBack<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Func<TSource, TSource> action
-		)
-		{
+		) {
 			for (int i = enumerable.Count() - 1; i >= 0; i--)
 				enumerable.ToArray()[i] = action(enumerable.ElementAt(i));
 			return enumerable.AsEnumerable();
@@ -99,8 +90,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> ForBack<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Func<TSource, int, TSource> action
-		)
-		{
+		) {
 			for (int i = enumerable.Count() - 1; i >= 0; i--)
 				enumerable.ToArray()[i] = action(enumerable.ElementAt(i), i);
 			return enumerable.AsEnumerable();
@@ -120,8 +110,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> SubArray<TSource>(
 			this IEnumerable<TSource> enumerable,
 			int startIndex, int length
-		)
-		{
+		) {
 			TSource[] final = new TSource[length];
 
 			Array.Copy(enumerable.ToArray(), startIndex, final, 0, length);
@@ -132,14 +121,12 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> SubArraySmart<TSource>(
 			this IEnumerable<TSource> enumerable,
 			int startIndex, int cycles, int stepsize = 1
-		)
-		{
+		) {
 			TSource[] final = new TSource[cycles];
 
 			int currentIndex = startIndex;
 
-			for (int i = 0; i < cycles; i++)
-			{
+			for (int i = 0; i < cycles; i++) {
 				//{TODO} Allow this to handle negative indexes
 				currentIndex =
 					MathE.ClampWrap(
@@ -165,21 +152,18 @@ namespace Excessives.LinqE
 		public static TSource Minimum<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Func<TSource, IComparable> selector
-			) where TSource : IComparable<TSource>
-		{
+			) where TSource : IComparable<TSource> {
 			TSource minimum = default(TSource);
 
 			bool assignedMinimum = false; //Used so we don't have to use a 'default' value
 
 			enumerable.ForEach(
-				n =>
-					{
-						if (selector(n).CompareTo(selector(minimum)) < 0 || !assignedMinimum)
-						{
-							minimum = n;
-							assignedMinimum = true;
-						}
+				n => {
+					if (selector(n).CompareTo(selector(minimum)) < 0 || !assignedMinimum) {
+						minimum = n;
+						assignedMinimum = true;
 					}
+				}
 				);
 
 			return minimum;
@@ -188,17 +172,14 @@ namespace Excessives.LinqE
 		public static TSource Maximum<TSource>(
 			this IEnumerable<TSource> enumerable,
 			Func<TSource, IComparable> selector
-			) where TSource : IComparable<TSource>
-		{
+			) where TSource : IComparable<TSource> {
 			TSource maximum = default(TSource);
 
 			bool assignedMaximum = false; //Used so we don't have to use a 'default' value
 
 			enumerable.ForEach(
-				n =>
-				{
-					if (selector(n).CompareTo(selector(maximum)) > 0 || !assignedMaximum)
-					{
+				n => {
+					if (selector(n).CompareTo(selector(maximum)) > 0 || !assignedMaximum) {
 						maximum = n;
 						assignedMaximum = true;
 					}
@@ -213,27 +194,6 @@ namespace Excessives.LinqE
 		#region Misc
 
 		/// <summary>
-		/// Returns the first element in an array that fits a given criteria
-		/// </summary>
-		/// <typeparam name="TSource"></typeparam>
-		/// <param name="enumerable"></param>
-		/// <param name="selector"></param>
-		/// <returns></returns>
-		public static TSource First<TSource>(
-			this IEnumerable<TSource> enumerable,
-			Func<TSource, bool> selector
-			)
-		{
-			using (var enumerator = enumerable.GetEnumerator())
-			{
-				while (enumerator.MoveNext())
-					if (selector(enumerator.Current))
-						return enumerator.Current;
-				return default(TSource);
-			}
-		}
-
-		/// <summary>
 		/// Selects the nth element in an enumerable
 		/// </summary>
 		/// <typeparam name="TSource"></typeparam>
@@ -243,13 +203,10 @@ namespace Excessives.LinqE
 		public static TSource Nth<TSource>(
 			this IEnumerable<TSource> enumerable,
 			int n
-			)
-		{
-			using (var enumerator = enumerable.GetEnumerator())
-			{
+			) {
+			using (var enumerator = enumerable.GetEnumerator()) {
 				int i = 0;
-				while (enumerator.MoveNext())
-				{
+				while (enumerator.MoveNext()) {
 					if (i == n)
 						return enumerator.Current;
 					i++;
@@ -268,12 +225,10 @@ namespace Excessives.LinqE
 		public static int FindIndex<TSource>(
 			this IEnumerable<TSource> enumerable,
 			TSource instance
-			)
-		{
+			) {
 			int i = 0;
 			var enumerator = enumerable.GetEnumerator();
-			while (enumerator.MoveNext())
-			{
+			while (enumerator.MoveNext()) {
 				if (enumerator.Current.Equals(instance))
 					return i;
 				i++;
@@ -285,8 +240,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> Swap<TSource>(
 			this IEnumerable<TSource> enumerable,
 			int index1, int index2
-			)
-		{
+			) {
 			//{TODO} Speed up! (This is the slowest 'swap' I have ever written...)
 			TSource[] array = enumerable.ToArray(); //Yuck!
 
@@ -301,8 +255,7 @@ namespace Excessives.LinqE
 		public static IEnumerable<TSource> SetAt<TSource>(
 			this IEnumerable<TSource> enumerable,
 			TSource value, int index
-			)
-		{
+			) {
 			if (index >= enumerable.Count())
 				throw new Exception("IndexOutOfRangeException"); //{TODO} Finish this
 
@@ -325,15 +278,13 @@ namespace Excessives.LinqE
 		/// <returns></returns>
 		public static TSource Pick<TSource>(
 			this IEnumerable<TSource> enumerable
-		)
-		{
+		) {
 			return CryptoRand.Pick(enumerable.ToArray());
 		}
 
 		public static IEnumerable<TSource> Shuffle<TSource>(
 			this IEnumerable<TSource> enumerable
-			)
-		{
+			) {
 			TSource[] newArray = new TSource[enumerable.Count()];
 
 			for (int i = 0; i < newArray.Length; i++)
